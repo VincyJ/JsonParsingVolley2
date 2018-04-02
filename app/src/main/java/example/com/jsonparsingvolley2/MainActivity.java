927b27c54e0cd51bfceeb1706599775db1d32954
Vincy <arputhavincyj@gmail.com>
2018-04-02 23:19:59 +0530

    Initial commit

---
+++ app/src/main/java/example/com/jsonparsingvolley2/MainActivity.java
@@ -0,0 +1,84 @@
+package example.com.jsonparsingvolley2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    //View to display names
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> namesList;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+        namesList = new ArrayList<>();
+        getJsonObjectRequest();
+    }
+
+    private void getJsonObjectRequest() {
+        String url = "https://siva123.000webhostapp.com/php/read_json.php";
+
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("VINCY", response.toString());
+
+                        try {
+                            // Parsing json array response
+                            // loop through each json object
+
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject person = (JSONObject) response.get(i);
+                                String name = person.getString("name");
+                                namesList.add(name);
+                            }
+                            // setting datas into the adapter
+                            adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, namesList);
+                            // setting adapter in the list
+                            listView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+    }
+}
